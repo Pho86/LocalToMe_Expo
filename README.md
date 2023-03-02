6754fa8e094bd2a3d8b90c0dfab6d844c6508d20
Pho <pho86@my.bcit.ca>
2023-03-01 19:49:11 -0800

    update readme

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ Can view the [Expo Project here](https://expo.dev/@localtome/LocalToMe).
 Expo application for LocalToMe, web application repo is [located here](https://github.com/phoenixlai833/LocalToMe).
 <br/>
 Learn more about LocalToMe in the [main repo's wiki](https://github.com/phoenixlai833/LocalToMe/wiki)!
+
+<div align="center">
+<img width="250" alt="local to me logo" src="/public/intro.png"/>
+<img width="250" alt="loader animation screen" src="/public/loader.png"/>
+</div>
+
+<div align="center">
+<img width="250" alt="onboarding screen" src="/public/onboarding.png"/>
+<img width="250" alt="map screen" src="/public/map.png"/>
+</div>
--- firebase/firebase.js
+++ firebase/firebase.js
@@ -4,22 +4,22 @@ import "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 import {
-    NEXT_PUBLIC_FIREBASE_API_KEY,
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_APP_ID
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID
 } from '@env';
 
 // Initialize Firebase with config
 const firebaseConfig = {
-    apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
